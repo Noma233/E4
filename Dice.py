d07a93d85d41ec73c7f5eb71e0bd3d6f90be57fa
noma233 <putongnor0@gmail.com>
2021-07-08 18:57:48 +0900

    Initial revision

---
+++ Dice.py
@@ -0,0 +1,12 @@
+import random 
+print('Rolling the dice...')
+
+x1 = random.randrange(5) + 1
+x2 = random.randrange(5) + 1
+
+print("Die 1: ", x1)
+print("Die 2: ", x2)
+
+print("Total value: ", x1 + x2)
+
+	
